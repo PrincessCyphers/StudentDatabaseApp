0a9a0edd395fd5991652de56b5cd68e1e1a31d07
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-12 18:58:07 +0000

    created array to take in number of students being enrolled, then calling student constructor for the required number of students. Minimises the main class and control of obj without multiple unecessary repeats

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,25 +1,25 @@
-/* Database admin for a uni & must create app to manage student enrollments & balance
-App should:
-- Ask user num of students to be added
-- Prompt user to enter name & year of student
-- Give student unique 5-digit ID with first number as their school year
-- Allow enrolment onto History 101, Mathematics 101, English 101, Chemistry 101, Computer Science 101
-- Show each course cost as £600
-- Allow student to view balance & pay tuition
-- Show student status - name, ID, course enrolled & balance
- */
+import java.util.Scanner;
 
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+
         // ask how mny students are being entered
-        Student stu1 = new Student();
-        stu1.enroll();
-        stu1.payTuition();
-        System.out.println(stu1.showInfo());
+        System.out.println("Enter number of new students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
+        // create n number of students. array will be used maintain the students
+        for (int n = 0; n < numOfStudents; n++) {
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payTuition();
+            System.out.println(students[n].showInfo());
+        }
 
 
 
-        // create n number of students
+
 
 
     }
