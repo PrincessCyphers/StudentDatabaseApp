050565456686be247c27f3c3bdf608ade9f6dc9e
PrincessMav <princess-iza@hotmail.co.uk>
2021-11-12 18:30:31 +0000

    calls to test new methods of enrol and, pay tuition and show info

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -15,6 +15,8 @@ public class StudentDatabaseApp {
         Student stu1 = new Student();
         stu1.enroll();
         stu1.payTuition();
+        System.out.println(stu1.showInfo());
+
 
 
         // create n number of students
